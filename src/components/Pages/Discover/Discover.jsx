5420985c2349afa5771787bd764300f034863cf5
Muditha <kabmmadusith2003@gmail.com>
2023-05-16 14:02:04 +0530

    Update discover section.

--- src/components/Pages/Discover/Discover.jsx
+++ src/components/Pages/Discover/Discover.jsx
@@ -10,11 +10,11 @@ export default function Discover() {
                 <img src={svg} alt="svg" />
             </div>
             <div className="textcont1">
-                <h5>PREMIUM BANK</h5>
-                <h2>Unlimited <br /> Transaction with <br /> zero fees</h2>
-                <p>Get access to our exclusive app that allows you <br /> to send unlimited transaction without getting <br /> charged any fees</p>
+                <h5>UNLIMITED ACCESS</h5>
+                <h2>Login to your <br /> account at any <br /> time.</h2>
+                <p>We have you covered no matter where you are <br /> located. All you need is an internet connection <br /> and mobile or computer.</p>
                 <div className="btm1">
-                    <button>Get Started</button>
+                    <button>Learn More</button>
                 </div>
             </div>
         </div>
